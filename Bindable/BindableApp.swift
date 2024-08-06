26b4b116f04dc55421bfcd53fbafc9cfe1fca80a
dave17code <dave17code@gmail.com>
2024-08-06 12:24:33 +0900

    Initial Commit

---
+++ Bindable/BindableApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindableApp.swift
+//  Bindable
+//
+//  Created by 김두운 on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BindableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
